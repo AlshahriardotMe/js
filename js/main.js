18d0696ee208dfc7b4225975b4556968a8ceadca
Abdullah al shahriar <103172072+Shahriarabo@users.noreply.github.com>
2022-12-01 12:44:19 +0600

    js-project

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function fcolor(){
+    document.getElementById("text")
+    .innerHTML ="This color name is: Red"
+}
+function gcolor(){
+    document.getElementById("text")
+    .innerHTML ="This color name is: Breen"
+}
+function bcolor(){
+    document.getElementById("text")
+    .innerHTML ="This color name is: Blue"
+}
+function ycolor(){
+    document.getElementById("text")
+    .innerHTML ="This color name is: Yellow"
+}
+function pcolor(){
+    document.getElementById("text")
+    .innerHTML ="This color name is: Pink"
+}
+function pcolor(){
+    document.getElementById("text")
+    .innerHTML ="This color name is: Pink"
+}
